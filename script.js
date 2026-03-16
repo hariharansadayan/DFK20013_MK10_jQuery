0f16d6d46ac647239789c0368d215a6d6b80d8ce
hariharansadayan <hariharansadayan@gmail.com>
2026-03-16 15:13:31 +0800

    First upload

---
+++ script.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+
+    $("#loginBtn").click(function () {
+
+        var username = $("#username").val();
+        var password = $("#password").val();
+
+        if (username == "" || password == "") {
+
+            $("#popupLogin").popup("open");
+
+        }
+        else {
+
+            $.mobile.changePage("#infoPage");
+
+        }
+
+    });
+
+});
